cd86a04ce1b8d74f5c07cc51f1148aba6e85861d
Esp Classroom <espuser@mit.edu>
2015-11-22 02:11:01 -0500

    Added example implementation

---
+++ implementation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from life_renderer import LifeWindow
+
+
+def setup(lifewindow):
+    pass
+
+
+def loop(lifewindow):
+    pass
+
+
+LifeWindow(10, 10, setup = setup, loop = loop)
